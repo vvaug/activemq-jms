3fd8328a5e1f9a4e8602896713f17981afebd659
vvaugusto <victorhyuuk1@gmail.com>
2020-02-12 00:20:06 -0200

    Topics

---
+++ jms/src/br/com/caelum/jms/TopicProducer.java
@@ -0,0 +1,41 @@
+package br.com.caelum.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TopicProducer {
+
+	/*
+	 * Author: Victor Silva
+	 */
+	
+	public static void main(String[] args) throws NamingException, JMSException {
+		
+		InitialContext context = new InitialContext();
+		Connection connection;
+		ConnectionFactory factory;
+		
+		factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		connection = factory.createConnection();
+		connection.start();
+		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination topic = (Destination) context.lookup("loja");
+		
+		MessageProducer producer = session.createProducer(topic);
+		
+		Message text = session.createTextMessage("Hey...new MESSAGE!");
+		
+		producer.send(text);
+		
+	}
+}
